90f32faa42a4e6c5ec8accdf31a068cd82fe48d6
Guilherme da Cruz Silveira <mail@guilhermecruz.com.br>
2020-04-09 16:12:11 -0300

    Create ngxpgsp.sh

---
+++ ngxpgsp.sh
@@ -0,0 +1,19 @@
+bash <(curl -f -L -sS https://ngxpagespeed.com/install)      --nginx-version latest
+
+> /lib/systemd/system/nginx.service
+
+echo "[Unit]" >>  /lib/systemd/system/nginx.service
+echo "Description=nginx - high performance web server" >>  /lib/systemd/system/nginx.service
+echo "Documentation=http://nginx.org/en/docs/" >>  /lib/systemd/system/nginx.service
+echo "After=network-online.target remote-fs.target nss-lookup.target" >>  /lib/systemd/system/nginx.service
+echo "Wants=network-online.target" >>  /lib/systemd/system/nginx.service
+echo " " >>  /lib/systemd/system/nginx.service
+echo "[Service]" >>  /lib/systemd/system/nginx.service
+echo "Type=forking" >>  /lib/systemd/system/nginx.service
+echo "PIDFile=/var/run/nginx.pid" >>  /lib/systemd/system/nginx.service
+echo "ExecStart=/usr/local/sbin/nginx -c /etc/nginx/nginx.conf" >>  /lib/systemd/system/nginx.service
+echo "ExecReload=/bin/kill -s HUP $MAINPID" >>  /lib/systemd/system/nginx.service
+echo "ExecStop=/bin/kill -s TERM $MAINPID" >>  /lib/systemd/system/nginx.service
+echo " " >>  /lib/systemd/system/nginx.service
+echo "[Install]" >>  /lib/systemd/system/nginx.service
+echo "WantedBy=multi-user.target" >>  /lib/systemd/system/nginx.service
